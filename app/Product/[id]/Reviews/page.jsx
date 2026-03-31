844c9767cc1cd73c1ac29096ca779a135a2fa282
Hastingamo <hezekiahhastings14@gmail.com>
2026-03-31 20:16:20 +0100

    feat: Add Review page component for product reviews

---
+++ app/Product/[id]/Reviews/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReviewForm from './ReviewForm'
+
+    export default  async function  page() {
+  return (
+    <div>
+        <h1> write  a review for this product</h1>
+        <ReviewForm  />
+    </div>
+  )
+}
