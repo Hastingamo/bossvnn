e06cabb32ad95fb9c2600cb8a80bf7ae5f7cec28
Hastingamo <hezekiahhastings14@gmail.com>
2026-04-25 11:57:05 +0100

    fix: Correct import path for createClient in Buy page component

--- app/Exchanges/Confirm/Buy/[id]/page.jsx
+++ app/Exchanges/Confirm/Buy/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use server";
 import Link from "next/link";
-import { createClient } from "../../../lib/server";
 import SellingDetails from "./BuyingDetails";
+import { createClient } from "../../../../lib/server";
 
 export default async function page({ params }) {
   const { id } = await params;
