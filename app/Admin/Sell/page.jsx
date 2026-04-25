5667b7118f847e4a76ef6d8fe979999a9983a803
Hastingamo <hezekiahhastings14@gmail.com>
2026-04-25 22:19:41 +0100

    yyyyd

--- app/Admin/Sell/page.jsx
+++ app/Admin/Sell/page.jsx
@@ -2,9 +2,9 @@
 import React from "react";
 import { createClient } from "../../lib/server";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
-
   const supabase = await createClient();
 
   const {
@@ -18,13 +18,14 @@ export default async function Page() {
       </div>
     );
   }
+if(role !== "admin"){
+  redirect("/");
+}
 
   const { data: transactions } = await supabase
     .from("transactions")
     .select("*")
     .order("created_at", { ascending: false });
-
-  const username = user.user_metadata?.username || "User";
 
   return (
     <div>
@@ -34,38 +35,36 @@ export default async function Page() {
         {!transactions || transactions.length === 0 ? (
           <p>No transactions yet.</p>
         ) : (
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 ">
-            {" "}
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
             {transactions.map((transaction) => (
               <div
                 key={transaction.id}
                 className="p-6 border rounded-lg shadow-sm bg-white w-full"
               >
                 <Link href={`/Admin/Sell/${transaction.id}`} className="text-lg font-semibold text-gray-900 dark:text-white mb-2">
-                <h2 className="text-xl font-bold text-gray-900 dark:text-white mb-2">
-                  {username}
-                </h2>
-                <p className="text-sm text-gray-600 mb-1">
-                  <span className="font-medium">Wallet ID:</span>{" "}
-                  {transaction.wallet_id}
-                </p>
-                <p className="text-sm text-gray-600 mb-1">
-                  <span className="font-medium">Wallet Address:</span>{" "}
-                  {transaction.wallet_address}
-                </p>
-                <p className="text-sm text-gray-600 mb-1">
-                  <span className="font-medium">Total NGN:</span>{" "}
-                  {transaction.amount}
-                </p>
-                <p className="text-sm text-gray-600 mb-1">
-                  <span className="font-medium">Currency:</span>{" "}
-                  {transaction.currency}
-                </p>
-
-                <p className="text-gray-900">{transaction.comment}</p>
-                <p className="text-sm text-gray-500 mt-2">
-                  {new Date(transaction.created_at).toLocaleDateString()}
-                </p>
+                  <h2 className="text-xl font-bold text-gray-900 dark:text-white mb-2">
+                    {transaction.username ?? "Anonymous"} 
+                  </h2>
+                  <p className="text-sm text-gray-600 mb-1">
+                    <span className="font-medium">Wallet ID:</span>{" "}
+                    {transaction.wallet_id}
+                  </p>
+                  <p className="text-sm text-gray-600 mb-1">
+                    <span className="font-medium">Wallet Address:</span>{" "}
+                    {transaction.wallet_address}
+                  </p>
+                  <p className="text-sm text-gray-600 mb-1">
+                    <span className="font-medium">Total NGN:</span>{" "}
+                    {transaction.amount}
+                  </p>
+                  <p className="text-sm text-gray-600 mb-1">
+                    <span className="font-medium">Currency:</span>{" "}
+                    {transaction.currency}
+                  </p>
+                  <p className="text-gray-900">{transaction.comment}</p>
+                  <p className="text-sm text-gray-500 mt-2">
+                    {new Date(transaction.created_at).toLocaleDateString()}
+                  </p>
                 </Link>
               </div>
             ))}
