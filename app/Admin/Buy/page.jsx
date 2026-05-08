4f651410aa64188362392c4f76ec5dc8be04094a
Hastingamo <hezekiahhastings14@gmail.com>
2026-05-08 02:16:03 +0100

    Change error logging from console.error to console.log in Buy and Sell pages

--- app/Admin/Buy/page.jsx
+++ app/Admin/Buy/page.jsx
@@ -136,7 +136,7 @@ export default async function Page() {
     .order("created_at", { ascending: false });
 
   if (error) {
-    console.error("Error fetching transfers:", error.message);
+    console.log("Error fetching transfers:", error.message);
   }
 
   return (
